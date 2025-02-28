b9bb72076ce3ba6b9a116a2c48eb1a99d273ceeb
oleg_dixon <olegdixon5973@gmail.com>
2025-02-28 14:42:19 +0500

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,29 +1,22 @@
+import random
+
 from brain_games import engine
-from brain_games.cli import welcome_user
-from brain_games.scripts.run_brain_games import greeting
+
+
+def generate_question_and_answer():
+    """ Generating a question for the "Calculator" game.
+    """
+    operators = ['+', '-', '*']
+    operator = random.choice(operators)
+    num_1 = random.randint(0, 100)
+    num_2 = random.randint(0, 100)
+    expression = f'{num_1} {operator} {num_2}'
+    correct_answer = eval(expression)
+    return expression, correct_answer
 
 
 def calc_game():
-    """ The main function of the game is "Calculator".
+    """ Launching the "Calculator" game.
     """
-    print(greeting())
-    user_name = welcome_user()
-    print('What is the result of the expression?')
-    correct_answers_need = 3
-    correct_answers_count = 0
-
-    while correct_answers_count < correct_answers_need:
-        _, generate_result = engine.generate_expression()
-        answer = engine.user_answer_integer()
-
-        if answer == generate_result:
-            print('Correct!')
-            correct_answers_count += 1
-        else:
-            print(f"{answer} is wrong answer;(. " 
-                  f"Correct answer was {generate_result}"
-                  f"\nLet's try again, {user_name}!")
-            break
-
-    if correct_answers_count == correct_answers_need:
-        print(f'Congratulations, {user_name}!')
+    description = 'What is the result of the expression?'
+    engine.run_game(description, generate_question_and_answer)
