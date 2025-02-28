c0204b7942320642fb9c00ea8647a8c28b2595dc
oleg_dixon <olegdixon5973@gmail.com>
2025-02-28 14:44:17 +0500

    add prime.py

---
+++ brain_games/games/prime.py
@@ -0,0 +1,28 @@
+import random
+
+from brain_games import engine
+
+
+def is_prime(number):
+    """A function to check if a number is prime."""
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+def generate_even_question():
+    """ Generating a question for "Is it a prime number?" game.
+    """
+    question = random.randint(1, 100)
+    correct_answer = 'yes' if is_prime(question) else 'no'
+    return question, correct_answer
+
+
+def prime_game():
+    """ Launching "Is it a prime number?" game.
+    """
+    description = "Answer 'yes' if given number is prime. Otherwise answer 'no'."
+    engine.run_game(description, generate_even_question)
