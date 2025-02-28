2d0b034750db786a06554ecaa5a7148ea60489b0
oleg_dixon <olegdixon5973@gmail.com>
2025-02-28 18:06:08 +0500

    fix brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,13 +2,13 @@ from brain_games.cli import welcome_user
 
 
 def greeting():
-    print('Welcome to the Brain Games!')
+    return 'Welcome to the Brain Games!'
 
 
 def main():
-    greeting()
+    print(greeting())
     welcome_user()
 
 
-if __name__ == '__main___':
+if __name__ == '__main__':
     main()
