3f38b471873f73e7dd919444fa5d3b8bd8633e86
oleg_dixon <olegdixon5973@gmail.com>
2025-02-28 14:42:36 +0500

    fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,36 +1,18 @@
+import random
+
 from brain_games import engine
-from brain_games.cli import welcome_user
-from brain_games.scripts.run_brain_games import greeting
+
+
+def generate_even_question():
+    """ Generating a question for the "Parity Check" game.
+    """
+    number = random.randint(0, 100)
+    correct_answer = 'yes' if number % 2 == 0 else 'no'
+    return number, correct_answer
 
 
 def even_game():
-    """ The main function of the game is "Parity check".
+    """ Launching the "Parity Check" game.
     """
-    print(greeting())
-    user_name = welcome_user()
-    print("Answer 'yes' if the number is even, otherwise answer 'no'.")
-    correct_answers_need = 3
-    correct_answers_count = 0
-
-    while correct_answers_count < correct_answers_need:
-        number = engine.generate_number()
-        answer = engine.user_answer_string()
-
-        if answer not in ['yes', 'no']:
-            print(f"Input incorrect! Let's try again, {user_name}!")
-            break
-
-        is_number_even = engine.is_even(number)
-        correct_answer = 'yes' if is_number_even else 'no'
-
-        if answer == correct_answer:
-            print('Correct!')
-            correct_answers_count += 1
-        else:
-            print(f"{answer} is wrong answer ;(."
-                  f"Correct answer was {correct_answer}"
-                  f"\nLet's try again, {user_name}!")
-            break
-
-    if correct_answers_count == correct_answers_need:
-        print(f'Congratulations, {user_name}!')
+    description = "Answer 'yes' if the number is even, otherwise answer 'no'."
+    engine.run_game(description, generate_even_question) 
