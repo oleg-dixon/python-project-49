39a29087c52b30b036df48b101224a2c3c818e66
oleg_dixon <olegdixon5973@gmail.com>
2025-03-17 23:48:05 +0500

    fix __init__.py

---
+++ brain_games/scripts/__init__.py
@@ -0,0 +1,11 @@
+from . import brain_calc, brain_even, brain_games, brain_gcd, brain_prime, brain_progression
+
+
+__all__ = (
+    'brain_calc',
+    'brain_even',
+    'brain_games',
+    'brain_gcd',
+    'brain_prime',
+    'brain_progression'
+)
