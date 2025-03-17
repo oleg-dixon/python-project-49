95b124f3a724f600a3a42c31fcef978a98f22e64
oleg_dixon <olegdixon5973@gmail.com>
2025-03-17 23:45:58 +0500

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,22 +1,33 @@
 import random
 
-from brain_games import engine
+
+DESCRIPTION = 'What is the result of the expression?'
 
 
-def generate_question_and_answer():
-    """ Generating a question for the "Calculator" game.
+def generate_round():
+    """ 
+    Generates a question for the "Calculator" game.
+
+    Returns:
+        str: A string representing the arithmetic expression (e.g., "3 + 5").
     """
     operators = ['+', '-', '*']
     operator = random.choice(operators)
     num_1 = random.randint(0, 100)
     num_2 = random.randint(0, 100)
-    expression = f'{num_1} {operator} {num_2}'
-    correct_answer = eval(expression)
-    return expression, correct_answer
+    question = f'{num_1} {operator} {num_2}'
+    return question
 
 
-def calc_game():
-    """ Launching the "Calculator" game.
+def generate_answer(question):
+    """ 
+    Calculates the correct answer for the given arithmetic expression.
+
+    Args:
+        question (str): The arithmetic expression to evaluate (e.g., "3 + 5").
+
+    Returns:
+        int: The result of the arithmetic expression.
     """
-    description = 'What is the result of the expression?'
-    engine.run_game(description, generate_question_and_answer)
+    correct_answer = eval(question)
+    return correct_answer
