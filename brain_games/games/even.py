616ecdfca3211cf2652060cba57274c9a8fa799e
oleg_dixon <olegdixon5973@gmail.com>
2025-03-17 23:46:30 +0500

    fix even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,18 +1,28 @@
 import random
 
-from brain_games import engine
+
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def generate_even_question():
-    """ Generating a question for the "Parity Check" game.
+def generate_round():
+    """ 
+    Generates a question for the "Parity Check" game.
+
+    Returns:
+        int: A random integer between 0 and 100.
     """
-    number = random.randint(0, 100)
-    correct_answer = 'yes' if number % 2 == 0 else 'no'
-    return number, correct_answer
+    question = random.randint(0, 100)
+    return question
 
 
-def even_game():
-    """ Launching the "Parity Check" game.
+def generate_answer(question):
+    """ 
+    Determines if the given number is even.
+
+    Args:
+        question (int): The number to check for parity.
+
+    Returns:
+        str: 'yes' if the number is even, otherwise 'no'.
     """
-    description = 'Answer "yes" if the number is even, otherwise answer "no".'
-    engine.run_game(description, generate_even_question) 
+    return 'yes' if question % 2 == 0 else 'no'
