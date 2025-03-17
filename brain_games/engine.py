1fe8d5f06efff71dc5873fc7278d1afe20c989df
oleg_dixon <olegdixon5973@gmail.com>
2025-03-17 23:45:04 +0500

    fix engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,24 +1,26 @@
 import prompt
 
-from brain_games.scripts.brain_games import greeting
+
+CORRECT_ANSWERS_NEED = 3
 
 
-def run_game(game_description, generate_question_and_answer):
-    """ A common game engine for all games.
-        :param game_description: Description of the game (string)
-        :param generate_question_and_answer: A function that 
-        generates a question and a correct answer
+def run_game(game_module):
+    """ A universal game engine.
+    Accepts the game module, which should contain:
+    - DESCRIPTION: description of the game
+    - generate_round(): a function that returns a question
+    - generate_answer(question): a function that returns the correct answer
     """
-    print(greeting())
+    print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
-    print(game_description)
+    print(game_module.DESCRIPTION)
     
-    correct_answers_need = 3
     correct_answers_count = 0
 
-    while correct_answers_count < correct_answers_need:
-        question, correct_answer = generate_question_and_answer()
+    while correct_answers_count < CORRECT_ANSWERS_NEED:
+        question = game_module.generate_round()
+        correct_answer = game_module.generate_answer(question)
         print(f'Question: {question}')
         user_answer = prompt.string('You answer: ')
 
@@ -31,5 +33,5 @@ def run_game(game_description, generate_question_and_answer):
                   f"\nLet's try again, {user_name}!")
             break
 
-    if correct_answers_count == correct_answers_need:
+    if correct_answers_count == CORRECT_ANSWERS_NEED:
         print(f'Congratulations, {user_name}!')
