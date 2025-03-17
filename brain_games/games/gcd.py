a1114fc9017019514bcce982f491e1366726627b
oleg_dixon <olegdixon5973@gmail.com>
2025-03-17 23:46:44 +0500

    fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,24 +1,36 @@
 import random
 
-from brain_games import engine
+
+DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
-def generate_even_question():
-    """ Generating a question for "The greatest common divisor" game.
+def generate_round():
+    """
+    Generates a question for the "Greatest Common Divisor" game.
+
+    Returns:
+        str: A string containing two random integers between 0 and 100, separated by a space.
     """
     num_1 = random.randint(0, 100)
     num_2 = random.randint(0, 100)
     question = f'{num_1} {num_2}'
+    return question
+
+
+def generate_answer(question):
+    """ 
+    Calculates the greatest common divisor (GCD) of two numbers.
+
+    Args:
+        question (str): A string containing two integers separated by a space.
+
+    Returns:
+        int: The greatest common divisor of the two numbers.
+    """
+    num_1, num_2 = map(int, question.split())
 
     while num_2 != 0:
         num_1, num_2 = num_2, num_1 % num_2
     correct_answer = num_1
-
-    return question, correct_answer
-
-
-def gcd_game():
-    """ Launching "The greatest common divisor" game.
-    """
-    description = 'Find the greatest common divisor of given numbers.'
-    engine.run_game(description, generate_even_question)
+    
+    return correct_answer
